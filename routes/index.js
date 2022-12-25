219c00dbc33a61c512e625198c575dde5d92a485
singhzrepo <contactwithsid@gmail.com>
2022-12-25 11:04:41 +0530

    Added profile APi

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express')
+const passport = require('passport')
 const router = express.Router()
 
 router.use('/client', require('./client')  )
 router.use('/customer', require('./customer'))
 router.use('/food', require('./food'))
+
+router.get('/profile', passport.authenticate('jwt', {failureRedirect: '/login', session: false}),
+    (req, res) => {
+        if(req.user){
+            return res.status(200).json({
+                data: req.user,
+                type: 'client',
+                message: "Client successfully found!"
+            })
+        }
+        return res.status(401).json({
+            data: {},
+            message: "Client not found!"
+        })
+    }
+)
 
 
 module.exports = router
